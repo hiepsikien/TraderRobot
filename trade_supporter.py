62630b03c99ced9af2c8dc6755f96b9b85dc9536
Andy Ng <anh.nguyendinh.cs@gmail.com>
2022-12-15 07:08:03 +0700

    first commit

---
+++ trade_supporter.py
@@ -0,0 +1,79 @@
+from datetime import datetime, timedelta
+from xml.dom.xmlbuilder import _DOMInputSourceCharacterStreamType
+import pandas as pd
+import time
+
+class BaseTrader():
+
+    def get_most_recent(self, symbol, interval, days):
+        now = datetime.utcnow()
+        past = str(now - timedelta(days = days))
+    
+        bars = self.client.futures_historical_klines(symbol = symbol, interval = interval,
+                                            start_str = past, end_str = None, limit = 1000) # Adj: futures_historical_klines
+        df = pd.DataFrame(bars)
+        df["Date"] = pd.to_datetime(df.iloc[:,0], unit = "ms")
+        df.columns = ["Open Time", "Open", "High", "Low", "Close", "Volume",
+                      "Clos Time", "Quote Asset Volume", "Number of Trades",
+                      "Taker Buy Base Asset Volume", "Taker Buy Quote Asset Volume", "Ignore", "Date"]
+        df = df[["Date", "Open", "High", "Low", "Close", "Volume"]].copy()
+        df.set_index("Date", inplace = True)
+        for column in df.columns:
+            df[column] = pd.to_numeric(df[column], errors = "coerce")
+        df["Complete"] = [True for row in range(len(df)-1)] + [False]
+        
+        self.data = df
+    
+    
+    def stream_candles(self, msg):
+        
+        # extract the required items from msg
+        event_time = pd.to_datetime(msg["E"], unit = "ms")
+        start_time = pd.to_datetime(msg["k"]["t"], unit = "ms")
+        first   = float(msg["k"]["o"])
+        high    = float(msg["k"]["h"])
+        low     = float(msg["k"]["l"])
+        close   = float(msg["k"]["c"])
+        volume  = float(msg["k"]["v"])
+        complete=       msg["k"]["x"]
+        
+        # print out
+        print(".", end = "", flush = True) 
+    
+        # feed df (add new bar / update latest bar)
+        self.data.loc[start_time] = [first, high, low, close, volume, complete]
+        
+        # prepare features and define strategy/trading positions whenever the latest bar is complete
+        if complete == True:
+            self.do_when_candle_closed()
+
+    def report_trade(self, order, going): # Adj!
+        
+        time.sleep(0.1)
+        order_time = order["updateTime"]
+        trades = self.client.futures_account_trades(symbol = self.symbol, startTime = order_time)
+        order_time = pd.to_datetime(order_time, unit = "ms")
+        
+        # extract data from trades object
+        df = pd.DataFrame(trades)
+        columns = ["qty", "quoteQty", "commission","realizedPnl"]
+        for column in columns:
+            df[column] = pd.to_numeric(df[column], errors = "coerce")
+        base_units = round(df.qty.sum(), 5)
+        quote_units = round(df.quoteQty.sum(), 5)
+        commission = -round(df.commission.sum(), 5)
+        real_profit = round(df.realizedPnl.sum(), 5)
+        price = round(quote_units / base_units, 5)
+        
+        # calculate cumulative trading profits
+        self.cum_profits += round((commission + real_profit), 5)
+        
+        # print trade report
+        print(2 * "\n" + 100* "-")
+        print("{} | {}".format(order_time, going)) 
+        print("{} | Base_Units = {} | Quote_Units = {} | Price = {} ".format(order_time, base_units, quote_units, price))
+        print("{} | Profit = {} | CumProfits = {} ".format(order_time, real_profit, self.cum_profits))
+        print(100 * "-" + "\n")
+
+    def do_when_candle_closed():
+        print("Action when candle completed: UNIMPLEMENTED")
